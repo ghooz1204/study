9c796b92f259d3d15b178c9b11910c8c7c332956
ghooz1204 <ghooz1204@naver.com>
2021-01-27 00:09:17 +0900

    # 기모링북 알파버전

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+
+    var box = document.querySelector('.box');
+
+    box.addEventListener('click', function (e) {
+        e.target.classList.add('moved');
+    });
+}
+// 기모링 북 업데이트 완.
+// GIMORING BOOK Beta V1.0
